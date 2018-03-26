380e302cefe7bd2de58e2efea8b540b10b2df57a
Alex Bakoushin <alex@bakoush.in>
2018-03-27 00:53:45 +0300

    Refactor deployment script

--- deploy_heroku.sh
+++ deploy_heroku.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export REPO=registry.heroku.com/shri-node-production/web
+export REPO=registry.heroku.com/shri-node-$MODE/web
 docker build --tag $REPO .
 docker login --username=_ --password=$HEROKU_AUTH registry.heroku.com
 docker push $REPO
