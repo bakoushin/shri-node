f17233386df1b1028c123d6b8fd632fa8f6e2286
Alex Bakoushin <alex@bakoush.in>
2018-03-27 14:37:44 +0300

    Add SauceLabs integration

--- .hermione.conf.js
+++ .hermione.conf.js
@@ -1,6 +1,22 @@
+const {hostname, port} = require('./config');
+
+const localUrl = {
+  base: `http://${hostname}:${port}`,
+  grid: 'http://0.0.0.0:4444/wd/hub'
+};
+
+const travisUrl = {
+  base: `${process.env.LT_SUBDOMAIN}.localtunnel.me`,
+  grid: `http://bakoushin:${process.env.SAUCELABS_AUTH}@ondemand.saucelabs.com:80/wd/hub`
+};
+
+const url = process.env.TRAVIS
+  ? travisUrl
+  : localUrl;
+
 module.exports = {
-  baseUrl: 'http://0.0.0.0:3000',
-  gridUrl: 'http://0.0.0.0:4444/wd/hub',
+  baseUrl: url.base,
+  gridUrl: url.grid,
   browsers: {
     chrome: {
       desiredCapabilities: {
