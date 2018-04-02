1f8cb9d32bae896fa9f17bccdfad608483d2f023
Alex Bakoushin <alex@bakoush.in>
2018-04-02 15:23:13 +0300

    Fix date testing timezone issue

--- test/git.test.js
+++ test/git.test.js
@@ -2,6 +2,8 @@
 
 const rewire = require('rewire');
 const {expect} = require('chai');
+const moment = require('moment');
+const config = require('../config');
 
 const git = rewire('../utils/git');
 
@@ -323,23 +325,27 @@ fc72ed61752302acded4f8ce8ae631a718d1987e|Mon, 22 Jan 2018 18:02:51 -0800|Sam Sac
       return Promise.resolve(gitOutput);
     });
 
+    function formatDate(date) {
+      return moment(date).format(config.dateFormat);
+    }
+
     const expectedResult = [
       {
-        date: '21.03.18 15:03:00',
+        date: formatDate('Wed, 21 Mar 2018 14:49:18 +0200'),
         email: 'fadysamirsadek@live.com',
         id: '62b950208d918925fda5204c435da0a2e14e540a',
         name: 'Fady Samir Sadek',
         subject: 'Flight: Cancel editing on ESCAPE key press (#1855)'
       },
       {
-        date: '16.03.18 21:03:00',
+        date: formatDate('Fri, 16 Mar 2018 20:15:08 +0200'),
         email: 'noreply@github.com',
         id: '047b1293b76b8a9c0e4908052e19ccc68aa5cafe',
         name: 'GitHub',
         subject: 'added npm-run-all to the package.json file (#1865)'
       },
       {
-        date: '16.03.18 17:03:00',
+        date: formatDate('Fri, 16 Mar 2018 16:36:55 +0200'),
         email: 'noreply@github.com',
         id: '179a2825fca1c0a160f078583620c7aee1358974',
         name: 'GitHub',
@@ -347,21 +353,21 @@ fc72ed61752302acded4f8ce8ae631a718d1987e|Mon, 22 Jan 2018 18:02:51 -0800|Sam Sac
           'disable cypress recording till the creation of a CYPRESS_RECORD_KEY (#1864)'
       },
       {
-        date: '16.03.18 17:03:00',
+        date: formatDate('Fri, 16 Mar 2018 16:02:09 +0200'),
         email: 'noreply@github.com',
         id: '215af94a43b19a76b0c5a795e6ec3509f7e5bbc1',
         name: 'GitHub',
         subject: 'Cypress test suite  <work in progress> (#1861)'
       },
       {
-        date: '07.03.18 18:03:00',
+        date: formatDate('Wed, 7 Mar 2018 17:38:53 +0200'),
         email: 'fadysamirsadek@live.com',
         id: '2cda67b20c555f76a67dc596f06d1721f2dd4816',
         name: 'Fady Samir Sadek',
         subject: 'Merge(update projects to todomvc-app-css v2 (#1811))'
       },
       {
-        date: '07.03.18 16:03:00',
+        date: formatDate('Wed, 7 Mar 2018 15:07:09 +0200'),
         email: 'fadysamirsadek@live.com',
         id: '544a0a52a684e231d093f4927f4eda8a94402509',
         name: 'Fady Samir Sadek',
@@ -369,21 +375,21 @@ fc72ed61752302acded4f8ce8ae631a718d1987e|Mon, 22 Jan 2018 18:02:51 -0800|Sam Sac
           '(Merge)Learn page to display all the learn.json info in one place (#1552)'
       },
       {
-        date: '03.03.18 05:03:00',
+        date: formatDate('Sat, 3 Mar 2018 04:08:06 +0200'),
         email: 'fadysamirsadek@live.com',
         id: 'c5bee456a812e75162f6ef5a5f380e8e5c312456',
         name: 'Fady Samir Sadek',
         subject: 'Change command to \'npm run start\' (#1807)'
       },
       {
-        date: '08.02.18 20:02:00',
+        date: formatDate('Thu, 8 Feb 2018 09:16:58 -0800'),
         email: 'samccone@gmail.com',
         id: '34e9739bd441408d36902e08d8ab8a69f6c5dc21',
         name: 'Sam Saccone',
         subject: 'Docs: Fix typo (#1849)'
       },
       {
-        date: '23.01.18 05:01:00',
+        date: formatDate('Mon, 22 Jan 2018 18:02:51 -0800'),
         email: 'samccone@gmail.com',
         id: 'fc72ed61752302acded4f8ce8ae631a718d1987e',
         name: 'Sam Saccone',
