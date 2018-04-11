bc529a7f55ba02c7510ff496bd87554d728bde65
Alex Bakoushin <alex@bakoush.in>
2018-04-11 10:35:48 +0300

    Get rid of unnecessary script

--- deploy_heroku.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-export REPO=registry.heroku.com/shri-node-$MODE/web
-docker build --tag $REPO .
-docker login --username=_ --password=$HEROKU_AUTH registry.heroku.com
-docker push $REPO
